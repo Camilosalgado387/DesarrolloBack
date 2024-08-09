89be5529a458356af849736ff5421e6cb89ec9fd
Camilo <camilosalgado@uninorte.edu.co>
2024-08-09 18:49:00 -0500

    Update taller1.js

--- taller1.js
+++ taller1.js
@@ -1,1 +1,19 @@
 
+//PUNTO 1
+function convertirTemp(c){
+	
+  return ((c*9)/5) + 32
+
+}
+
+//console.log('La temperatura en fahrenheit es: ' + convertirTemp(32))
+
+
+//PUNTO 2
+
+function resolvedor(a,b,c){
+
+	return 
+  
+}
+
