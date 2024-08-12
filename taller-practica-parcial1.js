54f77ede3576c05e753d12a6f86623966f8644b4
Camilo <camilosalgado@uninorte.edu.co>
2024-08-12 16:07:42 -0500

    Update taller-practica-parcial1.js

--- taller-practica-parcial1.js
+++ taller-practica-parcial1.js
@@ -1,21 +1,39 @@
 //punto 1
 
-function contarStringsLargos(lista) {
+function contarStrings(lista) {
     let cantidad = 0;
     for (let i = 0; i < lista.length; i++) {
         if (lista[i].length > 25) {
-            cantidad += 1;
+            cantidad += 1
         }
     }
-    return cantidad;
+    return cantidad
 }
-
-
-let listaStrings = [
+let lista = [
     "Este es un string corto",
     "Este es un string que tiene más de veinticinco caracteres",
     "Otro string corto",
     "Un string extremadamente largo que definitivamente tiene más de veinticinco caracteres"
 ];
 
-console.log(contarStringsLargos(listaStrings));  // Debería imprimir 2
+//console.log(contarStrings(lista));
+
+//punto 3
+function ocurrencias(lista, numero){	
+  let ocurrencia = 0
+  for(let i = 0; i <= lista.length; i++){
+  	if(lista[i] === numero)
+  		ocurrencia+= 1
+  }
+return ocurrencia
+}
+
+
+let lista = [1,1,1,1,2,2,3,3,3,4,4,5,6,7,8] 
+
+//console.log(ocurrencias(lista, 8))
+
+
+//Punto 4
+
+
