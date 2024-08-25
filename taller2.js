b6044a3743a238f90e4bb2e4212f7a64061f80bd
Camilo <camilosalgado@uninorte.edu.co>
2024-08-25 12:20:49 -0500

    Update taller2.js

--- taller2.js
+++ taller2.js
@@ -1,1 +1,45 @@
+//Punto 1
+function findMax(lista){
 
+	let mayor = lista[0]
+  for (let i = 0; i<lista.length; i++){
+  
+  	if(lista[i] > mayor){
+    mayor = lista[i]
+    }
+  }
+  return mayor
+}
+
+//Punto 2
+function includes(lista,valor){
+	for(let i= 0; i<lista.length; i++){
+  	if(lista[i] === valor){
+    	return true
+    }
+  
+  }	
+	return false
+}
+
+//Punto 3
+function sum(lista){
+	let suma = 0
+	for(let i = 0; i<lista.length; i++){
+    suma += lista[i]
+  }
+	return suma
+}
+
+//Punto 4 
+
+function  missingNumbers(lista){
+	let faltantes = []
+  lista.sort((a, b) => a - b)
+  for(let i = lista[0]; i < lista[lista.length-1];i++){
+  	if(!lista.includes(i)){
+    	faltantes.push(i)
+    }
+  }
+  return faltantes
+}
