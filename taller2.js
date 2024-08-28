c7dec508451d20de601e42ed169d78b8fe516c01
Camilo <camilosalgado@uninorte.edu.co>
2024-08-28 09:02:21 -0500

    Update taller2.js

--- taller2.js
+++ taller2.js
@@ -1,24 +1,21 @@
 //Punto 1
 function findMax(lista){
-
 	let mayor = lista[0]
-  for (let i = 0; i<lista.length; i++){
-  
-  	if(lista[i] > mayor){
-    mayor = lista[i]
-    }
-  }
-  return mayor
+	for (let i = 0; i<lista.length; i++){
+		if(lista[i] > mayor){
+	    	mayor = lista[i]
+		}
+	}
+	return mayor
 }
 
 //Punto 2
 function includes(lista,valor){
 	for(let i= 0; i<lista.length; i++){
-  	if(lista[i] === valor){
-    	return true
-    }
-  
-  }	
+  		if(lista[i] === valor){
+    			return true
+   		 }
+ 	 }	
 	return false
 }
 
@@ -26,8 +23,8 @@ function includes(lista,valor){
 function sum(lista){
 	let suma = 0
 	for(let i = 0; i<lista.length; i++){
-    suma += lista[i]
-  }
+    		suma += lista[i]
+  	}
 	return suma
 }
 
@@ -35,11 +32,11 @@ function sum(lista){
 
 function  missingNumbers(lista){
 	let faltantes = []
-  lista.sort((a, b) => a - b)
-  for(let i = lista[0]; i < lista[lista.length-1];i++){
-  	if(!lista.includes(i)){
-    	faltantes.push(i)
-    }
-  }
-  return faltantes
+  	lista.sort((a, b) => a - b)
+  	for(let i = lista[0]; i < lista[lista.length-1];i++){
+  		if(!lista.includes(i)){
+    			faltantes.push(i)
+    		}
+  	}
+ 	return faltantes
 }
