c374b0b8cf079fd62ffacb0a3399345190c69ef1
Camilo <camilosalgado@uninorte.edu.co>
2024-09-06 16:06:46 -0500

    Update taller3.js

--- taller3.js
+++ taller3.js
@@ -1,1 +1,25 @@
+//punto 1
 
+function desglosarString(string, tipo) {
+    let vocales = "aeiou"
+    let consonantes = "bcdfghjklmnpqrstvwxyz"
+    if(tipo === "vocales"){
+        return string.split('').filter(letra => vocales.includes(letra)).length
+    }else if(tipo === "consonantes"){
+        return string.split('').filter(letra => consonantes.includes(letra)).length
+    }
+}
+
+
+//Punto 2   
+function twoSum(nums, target) {
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+    return [];
+}
