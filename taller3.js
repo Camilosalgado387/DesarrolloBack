4e310d2a5b7f27179545b22af85b8ccbc876c6d5
Camilo <camilosalgado@uninorte.edu.co>
2024-09-06 20:10:13 -0500

    Update taller3.js
    Versión final

--- taller3.js
+++ taller3.js
@@ -12,17 +12,31 @@ function desglosarString(string, tipo) {
 
 
 //Punto 2   
-function twoSum(nums, target) {
-    let result = []
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i+1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                result.push(i)
-                result.push(j)
+function twoSum(lista, numero) {
+    let resultado = []
+    for (let i = 0; i < lista.length; i++) {
+        for (let j = i+1; j < lista.length; j++) {
+            if (lista[i] + lista[j] === numero) {
+                resultado.push(i)
+                resultado.push(j)
             }
         }
     }
-    return result
-    
+    return resultado
 }
+    
 //Punto 3
+function conversionRomana(romano) {
+    const dicRomano = {I: 1,V: 5,X: 10,L: 50,C: 100,D: 500,M: 1000}
+    let arabiga = 0
+    for (let i = 0; i < romano.length; i++) {
+        const dicActual = romano[i]
+        const dicSiguiente = romano[i + 1]
+        if (dicRomano[dicActual] < dicRomano[dicSiguiente]) {
+            arabiga -= dicRomano[dicActual]
+        } else {
+            arabiga += dicRomano[dicActual]
+        }
+    }
+    return arabiga
+}
